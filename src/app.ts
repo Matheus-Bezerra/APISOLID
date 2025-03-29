a2ac4cb24b6d0bf68e58c7c39c6b8daf063ae165
Matheus-BezerraaVanguard <matheus.bezerra@sistemavangurd.com.br>
2025-03-29 14:43:57 -0300

    feat: add Prisma and PostgreSQL setup with initial migrations and user model

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 
 export const app = fastify()  
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data: {
+        name: "Matheus Bezerra",
+        email: "matheusbwzerra002@gmail.com",
+        password_hash: ""
+    }
+})
